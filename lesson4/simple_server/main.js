68822013a91afc1359f4cbcfbbeb2cc672302d17
Dennis <58217156+allgeierke@users.noreply.github.com>
2021-04-16 16:40:27 +0200

    Add files via upload

---
+++ lesson4/simple_server/main.js
@@ -0,0 +1,21 @@
+const port = 3000,
+ http = require("http"),
+ httpStatus = require("http-status-codes"),
+ app = http.createServer();
+app.on("request", (req, res) => {
+  console.log(`Method: ${getJSONString(req.method)}`);
+  console.log(`Method: ${getJSONString(req.url)}`);
+  console.log(`Method: ${getJSONString(req.headers)}`);
+ res.writeHead(httpStatus.OK, {
+ "Content-Type": "text/html"
+ });
+ let responseMessage = "<h1>This will show on the screen.</h1>";
+ res.end(responseMessage);
+});
+app.listen(port);
+console.log(`The server has started and is listening on port number:
+➥ ${port}`);
+
+const getJSONString = obj => {
+ return JSON.stringify(obj, null, 2);
+};
